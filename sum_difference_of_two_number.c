ddca38cc011a90b11c8152e93df29570b082020f
satyendra12345 <43204084+satyendra12345@users.noreply.github.com>
2019-08-19 12:04:47 +0530

    Create sum_difference_of_two_number.c

---
+++ sum_difference_of_two_number.c
@@ -0,0 +1,15 @@
+int main()
+{int a,b,c,d;
+ float x,y,w,z;
+ scanf("%d %d",&a,&b);
+ c=a+b;
+ d=a-b;
+ printf("%d %d\n",c,d);
+ scanf("%f %f",&x,&y);
+ w=x+y;
+ z=x-y;
+ printf("%.1f %.1f",w,z);
+	
+    
+    return 0;
+}
