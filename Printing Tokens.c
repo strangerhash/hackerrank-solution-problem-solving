15ebda2669cc834413ab17758f47d650a978a9aa
satyendra12345 <43204084+satyendra12345@users.noreply.github.com>
2019-09-18 00:26:14 +0530

    Create Printing Tokens.c

---
+++ Printing Tokens.c
@@ -0,0 +1,16 @@
+//Printing Tokens
+int main() {
+
+     char *s, *t;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    
+    t = strtok(s, " ");
+    while (t != NULL) {
+        printf("%s\n", t);
+        t = strtok(NULL, " ");
+    }
+    
+    return 0;
+}
