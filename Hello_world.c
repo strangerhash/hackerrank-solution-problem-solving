7d4ecfb92b8364f70a3853f2657022b666c5baae
satyendra12345 <43204084+satyendra12345@users.noreply.github.com>
2019-09-18 00:32:59 +0530

    Create Hello_world.c

---
+++ Hello_world.c
@@ -0,0 +1,14 @@
+int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    char s[100]; 
+    printf("Hello, World.");
+    // Read a full line of input from stdin and save it to our variable, 
+    scanf("%[^\n]%*c",&s);
+    
+    
+    // Print a string literal saying "Hello, World." to stdout using pr
+printf("\n%s",s);
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
+    
+    return 0;
+}
